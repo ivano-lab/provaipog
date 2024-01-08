c619ae759768404aa62f160b71d75fe32bcff1c9
Ívano Fontes <ivanofnts@gmail.com>
2024-01-07 21:17:41 -0300

    tratamento cors

--- src/main/java/com/provaipog/todolist/controllers/TaskController.java
+++ src/main/java/com/provaipog/todolist/controllers/TaskController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/task")
 public class TaskController {
 
